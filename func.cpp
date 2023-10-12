03ef1705b14c2cdc1d487c31d2e313f4235e278f
Mirishkin Aleksandr <mirishkinaleksandr@gmail.com>
2023-10-13 00:17:02 +0300

    Add function MyMul()

--- func.cpp
+++ func.cpp
@@ -5,7 +5,13 @@ double MySum(double a, double b)
 {
 	return a + b;
 }
+
 double MySub(double a, double b)
 {
 	return a - b;
+}
+
+double MyMul(double a, double b)
+{
+return a * b;
 }
--- program.cpp
+++ program.cpp
@@ -9,6 +9,7 @@ int main()
 {
 	cout << MySum(10, 3) << endl;
 	cout << MySub(10, 3) << endl;
+	cout << MyMul(10, 3) << endl;
 	sistem("pause");
 	return 0;
 }
